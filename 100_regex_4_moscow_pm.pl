e9e27a6d74f49b8f458ea763828cffad88f780ed
Nikolay Mishin <mi@ya.ru>
2014-09-24 00:08:20 +0400

    удалил табы

--- 100_regex_4_moscow_pm.pl
+++ 100_regex_4_moscow_pm.pl
@@ -6,8 +6,8 @@ use Encode::Locale qw(decode_argv);
  if (-t) 
 {
     binmode(STDIN, ":encoding(console_in)");
-	binmode(STDOUT, ":encoding(console_out)");
-	binmode(STDERR, ":encoding(console_out)");
+    binmode(STDOUT, ":encoding(console_out)");
+    binmode(STDERR, ":encoding(console_out)");
 }
 
 Encode::Locale::decode_argv();
