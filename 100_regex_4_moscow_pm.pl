732f8d7fd5e67944f0092f57be20bae504d0948e
Nikolay Mishin <mi@ya.ru>
2014-09-24 00:05:27 +0400

    слава Encode::Locale::decode_argv();

--- 100_regex_4_moscow_pm.pl
+++ 100_regex_4_moscow_pm.pl
@@ -1,32 +1,19 @@
 ﻿#!/usr/bin/env perl
 use utf8;
 use Modern::Perl;
-use Encode::Locale;
-use Encode qw( decode encode from_to);
-use Encode::Detect::Detector;
-use Data::Dumper qw( Dumper ); 
+use Encode::Locale qw(decode_argv);
 
- 
-if (-t) 
+ if (-t) 
 {
     binmode(STDIN, ":encoding(console_in)");
 	binmode(STDOUT, ":encoding(console_out)");
 	binmode(STDERR, ":encoding(console_out)");
 }
 
+Encode::Locale::decode_argv();
 my $lang = shift or die "Usage: $0 What_is_your_language?\n"; 
-my_dump('lang_01',$lang);
-$lang = decode(locale => $lang);
-my_dump('lang_02',$lang);
-my_dump('lang_03_перл',qq{перл});
 
- $lang =~ /(perl|перл) (?{print "use Perl or die!!\nИспользуй Перл или умри!!";}) /ix;#русский не мачится, почему?
- 
- sub my_dump
- { 
- my ($name,$var)=@_;
- local $Data::Dumper::Useqq = 1;
- local $Data::Dumper::Indent = 0; 
- local $Data::Dumper::Terse = 1; 
- print(Encode::Detect::Detector::detect(qq{$lang})." ".qq{\$$name }." dump=".Dumper($var)."\n");
- }
+ $lang =~ /
+ (perl|перл) 
+ (?{print "use Perl or die!!\nИспользуй Перл или умри!!";}) 
+          /ix;
