fc920537af696bc805391c3f12a1570afa5663b1
Nikolay Mishin <mi@ya.ru>
2015-07-23 16:06:02 +0300

    добавил сортировку
    когда большие буквы идут впереди

--- sort_array.pl
+++ sort_array.pl
@@ -3,6 +3,7 @@ use Sort::Maker;
 my @array_of_hash = (
     {name => 'Moscow'},
     {name => 'Amsterdam'},
+    {name => 'AMsterdam'},
     {name => 'Bonn'},
     {name => 'New York'},
     {name => 'Vladivostok'}
@@ -20,6 +21,10 @@ my @sorted_array_of_hash = townSorter(@array_of_hash);
 print Dumper \@sorted_array_of_hash;
 
 
-my @sorted_array_of_hash2 =  sort { $a->{name} cmp $b->{name} } @array_of_hash;
+# my @sorted_array_of_hash2 =  sort { $a->{name} cmp $b->{name} } @array_of_hash;
+# $a->{name} cmp $b->{name}
+my @sorted_array_of_hash2 =
+  sort { (lc($a->{name}) cmp lc($b->{name})) or ($a->{name} cmp $b->{name}) }
+  @array_of_hash;
 
 print Dumper \@sorted_array_of_hash2;
