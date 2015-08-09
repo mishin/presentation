6448555f819a382e068d2285ed94e366972cf693
Nikolay Mishin <mi@ya.ru>
2015-08-09 09:46:00 +0300

    это оказалась степень двойки

--- 01_fork.pl
+++ 01_fork.pl
@@ -1,18 +1,17 @@
 #!/usr/bin/env perl
 use v5.10;
 
-my ( $init_start, $init_end )=@ARGV;
+my ( $init_start, $init_end ) = @ARGV;
 step_by_n( $init_start, $init_end );
 
 sub step_by_n {
     my ( $start, $end ) = @_;
-#        if ( $start > $end ) {
-#		exit;
-#        }
     say "start step_by_n($start,$end)";
     for my $k ( $start .. $end ) {
-        say "from step_by_n($start,$end): $k";
+        if ( $k < $end ) {
+            say "from step_by_n($start,$end): $k";
             step_by_n( $k + 1, $end );
+        }
 
         #fork();
     }
